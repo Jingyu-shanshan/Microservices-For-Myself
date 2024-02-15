84858e44afae3768633d35091712c45dcff2c517
wass <jingyu.liushanshan@outlook.com>
2024-02-15 22:33:27 +0200

    [AccountService] add dependencies

--- AccountService/build.gradle.kts
+++ AccountService/build.gradle.kts
@@ -10,6 +10,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java:8.0.31")
+
+    // unit test
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }

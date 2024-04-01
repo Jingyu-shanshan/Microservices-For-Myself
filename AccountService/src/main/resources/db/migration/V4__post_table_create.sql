d10e3f0186a8b64ce8d6db2be457de497b53e404
wass <jingyu.liushanshan@outlook.com>
2024-04-01 15:11:18 +0300

    [postService] use flyway to create post table.

---
+++ AccountService/src/main/resources/db/migration/V4__post_table_create.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255),
+  `description` varchar(255),
+  `content` varchar(255),
+  `type` varchar(255),
+  `created_at` DATE,
+  PRIMARY KEY (`id`)
+);
